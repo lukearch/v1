b235953060cb5967693e6ab9f9c390e0ae1891ea
Lucas Larangeira <luke@meuvidraceiro.com.br>
2023-10-19 22:56:09 -0300

    install analytics

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { AppModule } from "./app/app.module";
+import { inject } from "@vercel/analytics";
+import { environment } from "./environments/environment";
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
+  .then(() => {
+    inject({
+      mode: environment.production ? "production" : "development"
+    });
+  })
   .catch((err) => console.error(err));
