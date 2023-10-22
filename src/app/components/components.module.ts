de60cbe2f20e826a9a4dcf1eb9270bddbb8405bb
Lucas Larangeira <luke@meuvidraceiro.com.br>
2023-10-22 17:16:07 -0300

    switch language to en-US

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,20 +4,19 @@ import { FormsModule } from "@angular/forms";
 import { ChipModule } from "primeng/chip";
 import { DropdownModule } from "primeng/dropdown";
 import { TimelineModule } from "primeng/timeline";
+import { IntersectionObserverDirective } from "../directives/intersection-observer/intersection-observer.directive";
 import { TransitionDirective } from "../directives/transition/transition.directive";
+import { ChipComponent } from "./chip/chip.component";
+import { ContentSectionComponent } from "./content-section/content-section.component";
 import { ExperienceCardComponent } from "./experience-card/experience-card.component";
 import { ExperienceTimelineComponent } from "./experience-timeline/experience-timeline.component";
+import { GlassCardComponent } from "./glass-card/glass-card.component";
 import { HeaderComponent } from "./header/header.component";
-import { LanguageSelectorComponent } from "./language-selector/language-selector.component";
 import { LoadingScreenComponent } from "./loading-screen/loading-screen.component";
 import { NavigationTimelineComponent } from "./navigation-timeline/navigation-timeline.component";
 import { NetworksComponent } from "./networks/networks.component";
 import { PointerGlowComponent } from "./pointer-glow/pointer-glow.component";
-import { ContentSectionComponent } from "./content-section/content-section.component";
-import { IntersectionObserverDirective } from "../directives/intersection-observer/intersection-observer.directive";
-import { GlassCardComponent } from "./glass-card/glass-card.component";
 import { ProjectCardComponent } from "./project-card/project-card.component";
-import { ChipComponent } from "./chip/chip.component";
 
 @NgModule({
   declarations: [
@@ -25,7 +24,6 @@ import { ChipComponent } from "./chip/chip.component";
     PointerGlowComponent,
     HeaderComponent,
     NavigationTimelineComponent,
-    LanguageSelectorComponent,
     NetworksComponent,
     ExperienceTimelineComponent,
     ExperienceCardComponent,
