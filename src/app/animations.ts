146658dcbc74178859c917ac119f0293f4733b7a
Lucas Larangeira <luke@meuvidraceiro.com.br>
2023-11-18 21:56:47 -0300

    update

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -9,37 +9,116 @@ import {
 } from "@angular/animations";
 
 export const fadeAnimation = trigger("routeAnimations", [
-  transition("* <=> *", [
+  transition("HomePage => *", [
+    style({
+      position: "relative"
+    }),
+    query(":enter, :leave", [
+      style({
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%"
+      })
+    ]),
+    query(
+      ":enter",
+      [
+        style({
+          left: "100%"
+        })
+      ],
+      { optional: true }
+    ),
+    query(":leave", animateChild(), { optional: true }),
     group([
       query(
         ":leave",
         [
           animate(
-            "200ms ease-out",
+            "300ms ease-out",
             style({
+              left: "-100%",
               opacity: 0
             })
           )
         ],
-        {
-          optional: true
-        }
+        { optional: true }
       ),
       query(
         ":enter",
         [
           animate(
-            "200ms ease-out",
+            "300ms ease-out",
             style({
-              opacity: "0"
+              left: "0%",
+              opacity: 1
             })
           )
         ],
-        {
-          optional: true
-        }
+        { optional: true }
+      )
+    ])
+  ]),
+  transition("* => HomePage", [
+    style({
+      position: "relative"
+    }),
+    query(":enter, :leave", [
+      style({
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%"
+      })
+    ]),
+    query(
+      ":enter",
+      [
+        style({
+          left: "-100%"
+        })
+      ],
+      { optional: true }
+    ),
+    query(":leave", animateChild(), { optional: true }),
+    group([
+      query(
+        ":leave",
+        [
+          animate(
+            "300ms ease-out",
+            style({
+              left: "100%"
+            })
+          ),
+          animate(
+            "100ms ease-out",
+            style({
+              opacity: 0
+            })
+          )
+        ],
+        { optional: true }
       ),
-      query("@*", animateChild(), { optional: true })
+      query(
+        ":enter",
+        [
+          animate(
+            "300ms ease-out",
+            style({
+              left: "0%"
+            })
+          ),
+          animate(
+            "100ms ease-out",
+            style({
+              opacity: 1
+            })
+          )
+        ],
+        { optional: true }
+      )
     ])
   ])
 ]);
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { ContactComponent } from "./contact/contact.component";
 import { HomeComponent } from "./home/home.component";
 import { ProjectsComponent } from "./projects/projects.component";
-import { ContactComponent } from "./contact/contact.component";
 
 const routes: Routes = [
   {
