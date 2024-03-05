cf6c1971ec4d3e6aebd42408e93bc4e7da260c9d
Lucas Larangeira <lukearch.sh@gmail.com>
2024-03-05 16:51:45 -0300

    fix import

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { inject } from "@vercel/analytics";
-import { injectSpeedInsights } from "@vercel/speed-insights/*";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
 
